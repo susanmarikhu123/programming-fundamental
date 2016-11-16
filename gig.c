2eec6a0d7332dd67574b3221e9b2102f44faa1aa
susanmarikhu123 <susan.marikhu123@gmail.com>
2016-11-16 11:11:08 +0545

    Add files via upload

---
+++ gig.c
@@ -0,0 +1,16 @@
+/* student name :susan marikhu
+subject : programming fundamental
+roll no. : 16
+lab no. : 01
+program : write a program to print your name.
+date :november 16,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main (){
+printf("susan marikhu");
+getch();
+return 0;
+
+}
