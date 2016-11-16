ca080efcb74808bc2294320f0a9de10713fc70f6
susanmarikhu123 <susan.marikhu123@gmail.com>
2016-11-16 10:56:07 +0545

    Add files via upload

---
+++ hub.c
@@ -0,0 +1,24 @@
+/* student name:susan marikhu
+subject : programming fundamental
+roll no: 16
+lab no: 02
+program: write a program to print product of two numbers asked from  users.
+date :november 16, 2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main (){
+int a ,b ,product;
+printf("enter first number\n");
+scanf("%d",&a);
+printf("enter second number\n ");
+scanf("%d",&b);
+product=a*b;
+printf("product of two number is:%d",product);
+
+getch();
+return 0;
+
+}
+
