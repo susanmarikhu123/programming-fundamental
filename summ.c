d31465f9fc9603579b1f8dcfcb714ef2bc9becb5
susanmarikhu123 <susan.marikhu123@gmail.com>
2016-11-16 10:59:57 +0545

    Add files via upload

---
+++ summ.c
@@ -0,0 +1,22 @@
+/* Student name : susan marikhu
+Subject : programming fundamental
+Roll no : 16
+Lab no: 02
+Program : write a c program to print sum of two numbers asked from users.
+Date: november 16,2016
+*/
+
+#include<stdio.h>
+#include <conio.h>
+ int main () {
+ int a,b,sum;
+ printf("enter first number \n");
+ scanf("%d",&a);
+ printf("enter second number \n");
+ scanf("%d",&b);
+ sum=a+b;
+ printf("sum of two number is:%d",sum);
+ getch();
+ return 0;
+
+ }
