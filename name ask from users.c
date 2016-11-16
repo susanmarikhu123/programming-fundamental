30a29b8e351fef43410639696f985990e5e51ca9
susanmarikhu123 <susan.marikhu123@gmail.com>
2016-11-16 11:32:34 +0545

    Add files via upload

---
+++ name ask from users.c
@@ -0,0 +1,17 @@
+/* student name :susan marikhu
+subject: programming fundamental
+roll no. :16
+lab no. :01
+program :write a program to print your name ask fro user.
+date :november 16,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main (){
+char name[20];
+printf("what is your name \n");
+scanf("%s",&name);
+printf("name of user is %s " ,name);
+getch();
+return 0;
+}
