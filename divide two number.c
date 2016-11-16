f9ac255fcfce5e233a9ef3b6dbe409a147ad55bd
susanmarikhu123 <susan.marikhu123@gmail.com>
2016-11-16 12:42:37 +0545

    Add files via upload

---
+++ divide two number.c
@@ -0,0 +1,23 @@
+
+/* student name :susan marikhu
+subject : programming fundamental
+roll no. :16
+lab no. :
+program : write a program to print divide of two number asked from users.
+date : november 16 ,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main (){
+float a,b,divide;
+printf ("enter first number\n");
+scanf( "%f",&a);
+printf("enter second number\n");
+scanf("%f",&b);
+divide = a/b;
+printf("divide of two number is :%f",divide);
+getch();
+return 0;
+
+}
