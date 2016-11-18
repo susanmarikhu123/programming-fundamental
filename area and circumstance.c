a2fcd2c5f5c94514256fac81dc34a5ff4cb63abf
susanmarikhu123 <susan.marikhu123@gmail.com>
2016-11-18 12:54:50 +0545

    Add files via upload

---
+++ area and circumstance.c
@@ -0,0 +1,23 @@
+/*
+Student name:Susan Marikhu
+subject:Programming Fundamental
+Roll no. :16
+Lab no. :03
+Program :Write a program to print simple interest
+Date:November 18,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main (){
+float r,area,circumstances,PI=3.14;
+printf("Enter The Radius:\n");
+scanf("%f",&r);
+area=PI*r*r;
+circumstances=2*PI*r;
+printf("\nThe area of radius is:%f",area);
+printf("\nThe circumstances of radius is:%f",circumstances);
+getch();
+return 0;
+
+}
