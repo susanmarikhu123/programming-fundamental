9e81a6fa9b8c49baeba47a913ba312a7c536def5
susanmarikhu123 <susan.marikhu123@gmail.com>
2016-11-23 11:48:30 +0545

    Add files via upload

---
+++ sum of three digits.c
@@ -0,0 +1,25 @@
+/*
+Student Name :Susan Marikhu
+Subject :Programming Fundamental
+Lab No. :05
+Program :Write a C program to find sum of digits of three digits number.
+Date: november 23,2016
+*/
+
+#include<stdio.h>
+int main() {
+int a,b,c,sum;
+printf("Enter The First Number:\n");
+scanf("%d",&a);
+printf("Enter The Second Number:\n");
+scanf("%d",&b);
+printf("Enter The Third Number:\n");
+scanf("%d",&c);
+sum=a+b+c;
+printf("The Sum Of Three Digits is :%d",sum);
+return 0;
+
+
+
+}
+
