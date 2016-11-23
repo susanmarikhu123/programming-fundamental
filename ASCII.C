d7ef19b4b8189db692513e2350cbca12eb92e17c
susanmarikhu123 <susan.marikhu123@gmail.com>
2016-11-23 13:08:00 +0545

    Add files via upload

---
+++ ASCII.C
@@ -0,0 +1,22 @@
+/*
+Student Name :susan  marikhu
+subject : programming fundamental
+Lab No.:05
+Program:write a C program to find roots of quadratic equation & value are assigned from users.
+date :november 23,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main (){
+char character;
+int ASCIIvalue;
+printf("Enter The Character:\n");
+scanf("%c",&character);
+ASCIIvalue=character;
+printf("the value of ASCII is:%d",ASCIIvalue);
+getch();
+return 0;
+
+
+}
