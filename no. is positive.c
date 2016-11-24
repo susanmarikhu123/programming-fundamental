5579a8ec8b65f53bac5ae2c6e30b577cccbc1da0
susanmarikhu123 <susan.marikhu123@gmail.com>
2016-11-24 11:21:52 +0545

    Add files via upload

---
+++ no. is positive.c
@@ -0,0 +1,22 @@
+/* Student Name : Susan Marikhu
+Subject:Programming  Fundamental
+Lab No.:06
+Program :Write a C program to print positive number asked from users.
+Date :November 24,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main (){
+int n;
+printf("Enter The Number:\n");
+scanf("%d",&n);
+if (n>0);
+printf("number is positive ");
+
+getch();
+return 0;
+}
+
+
+
