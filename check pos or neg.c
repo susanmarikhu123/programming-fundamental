cdd849642d4958f8bd940e9320b93aa0fec37a61
susanmarikhu123 <susan.marikhu123@gmail.com>
2016-11-25 11:57:40 +0545

    Add files via upload

---
+++ check pos or neg.c
@@ -0,0 +1,33 @@
+/*
+Student Name :susan  marikhu
+subject : programming fundamental
+Lab No.:06
+Program:write a C program to arithmatic operation.
+Date :november 25,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main (){
+int n;
+printf("Enter The Number:\n");
+scanf("%d",&n);
+if (n>0)
+{
+    printf("The number is positive ");
+}
+else if(n<0)
+{
+    printf ("The number is negative ");
+
+    }
+else{
+    printf("THE NUMBER IS ZERO");
+}
+
+getch();
+return 0;
+}
+
+
+
