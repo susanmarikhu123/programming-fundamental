cf29e1fc3757e90749b452bf6946a3fda6e59334
susanmarikhu123 <susan.marikhu123@gmail.com>
2016-11-25 10:55:20 +0545

    Add files via upload

---
+++ odd or even.c
@@ -0,0 +1,31 @@
+/*
+Student Name :susan  marikhu
+subject : programming fundamental
+Lab No.:06
+Program:write a C program to arithmatic operation.
+Date :november 25,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main (){
+    int a;
+    printf("Enter the number:");
+    scanf("%d",&a);
+
+
+    if(a%2==0) {
+        printf("the number is even:");
+    }
+      else
+
+    {
+    printf("the number is odd: ");
+
+    }
+
+    getch();
+    return 0;
+
+
+    }
