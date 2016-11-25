bb11930027d95456de7a49a655e0eb527dc20f48
susanmarikhu123 <susan.marikhu123@gmail.com>
2016-11-25 10:17:56 +0545

    Add files via upload

---
+++ arithematic operation.c
@@ -0,0 +1,44 @@
+/*
+Student Name :susan  marikhu
+subject : programming fundamental
+Lab No.:06
+Program:write a C program to arithmatic operation.
+Date :november 25,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a,b,sum,sub,divi,modulus,multi;
+printf("Enter the first number:\n");
+scanf("%d",&a);
+printf("Enter the second number:\n");
+scanf("%d",&b);
+sum=a+b;
+sub=a-b;
+divi=a/b;
+modulus=a%b;
+multi=a*b;
+printf("\n The sum of two number is:%d",sum);
+printf("\n The subtraction of two number is :%d",sub);
+printf("\n The division of two number is:%d",divi);
+printf("\n The modulus of two number is:%d",modulus);
+printf("\n The multiple of two number is:%d",multi);
+getch();
+return 0;
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
