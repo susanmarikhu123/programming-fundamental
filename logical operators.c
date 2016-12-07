154190dda20b3ef1accddb7c5955500759c8d1d8
susanmarikhu123 <susan.marikhu123@gmail.com>
2016-12-07 11:08:20 +0545

    Add files via upload

---
+++ logical operators.c
@@ -0,0 +1,27 @@
+/*
+Student Name : Susan Marikhu
+Subject : Programming Fundamental
+Lab Sheet no.:10
+Program : Write a C program to use logical operators.
+Date : December 07,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+ int main(){
+ int a;
+ printf(" Enter The Number:");
+ scanf("%d",&a);
+if(a>=80 && a<=100)
+    printf("\n\t Congratulation, You Have Passed In Distinction.",a);
+if(a>=60 && a<80)
+    printf("\n\t Congratulation, You Have Passed In First Division.",a);
+if(a>=45 && a<60)
+    printf("\n\t Congratulation,You Have Passed In Second Division.",a);
+if(a<=45)
+    printf("\n\t Work Hard, You Have Passed In Third Division.",a);
+
+getch();
+
+return 0;
+}
