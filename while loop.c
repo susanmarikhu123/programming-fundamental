492ca147f7e0fed4aa80efdba14631fa50aaab86
susanmarikhu123 <susan.marikhu123@gmail.com>
2017-01-02 10:50:38 +0545

    Add files via upload

---
+++ while loop.c
@@ -0,0 +1,24 @@
+/*
+Name:'Susan Marikhu
+Program:Bcs
+Lab  Sheet No.:15
+Subject:WAp to print all natural numbers from 1 to n using while loop.
+Date :Jan 2,2017
+*/
+
+#include<stdio.h>
+
+int main(){
+int a=1,n;
+printf("\n Enter Any Number:");
+scanf("%d",&n);
+
+while(a<=n)
+
+{
+	printf(" %d ",a++);
+
+}
+return 0;
+
+}
