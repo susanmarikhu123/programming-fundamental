6df0d322941021fc408a4671f4db319119b1261b
susanmarikhu123 <susan.marikhu123@gmail.com>
2017-01-16 10:19:40 +0545

    Add files via upload

---
+++ display 2nd pattern.c
@@ -0,0 +1,28 @@
+/*
+Student name: Susan Marikhu
+Subject:Programming Fundamental
+RollNo:45
+Program:Wap to display the pattern.
+Date:18th January ,2017
+*/
+
+
+#include<stdio.h>
+
+	int main(){
+	int i,j,n;
+	printf("\n Enter Any Number:");
+	scanf("%d",&n);
+	
+	for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=i;j++)
+			
+		{
+			printf("x");
+		}
+		printf("\n");
+		
+	}
+		return 0;
+		}
