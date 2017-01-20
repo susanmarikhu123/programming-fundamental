4af0d8e467ac2e67a1e2a85b6a8929446898d062
susanmarikhu123 <susan.marikhu123@gmail.com>
2017-01-20 12:57:29 +0545

    Add files via upload

---
+++ display pattern fifth.c
@@ -0,0 +1,36 @@
+/*
+Student Name :Susan Marikhu
+Subject :Programming Fundamental
+Lab Sheet No.:20
+Program:Wap to display pattern using nested if loop.
+Date:20 january ,2017
+*/
+#include<stdio.h>
+
+	int main(){
+		int n,i,j;
+		printf("Enter The Number:\n");
+		scanf("%d",&n);
+		 
+		 for (i=n;i>=1;i--)
+		 {
+			 for(j=1;j<=n;j++)
+			{
+				if(i>j)
+				{
+				printf(" ");
+			}
+			else
+			
+			
+			 {
+				 printf("*");
+			 }
+			
+			}
+			 printf("\n");
+		 }
+		
+		return 0;
+	}
+		
