631181744f835f5cd48c59744821103141c7150c
susanmarikhu123 <susan.marikhu123@gmail.com>
2017-02-01 11:09:31 +0545

    Add files via upload

---
+++ array.c
@@ -0,0 +1,19 @@
+/*
+Student Name :Susan Marikhu
+Subject:Programming Fundamental
+Lab Shet No.:22
+Program:Wap to display using array.
+Date:1st febuary,2017
+*/
+#include<stdio.h>
+int main()
+{
+    int a[10]={1,2,3,4,5,6,7,8,9,10};
+    int i;
+    for(i=0;i<10;i++)
+        {
+    printf("a[%d]=%d\n",i,a[i]);
+    }
+    return 0;
+
+}
