a31c12b93d94a5b9d18b07868da61cc7de794174
susanmarikhu123 <susan.marikhu123@gmail.com>
2017-02-06 10:43:21 +0545

    Add files via upload

---
+++ addition structure.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+struct sum
+{
+int a;
+int b;
+}result;
+int main()
+{
+int add;
+printf("Enter THe First number:");
+scanf("%d",&result.a);
+printf("Enter The second number:");
+scanf("%d",&result.b);
+	add=result.a+result.b;
+	printf("The Sum Is:%d",add);
+	return 0;
+}
+
+	
+
+	
--- structure.c
+++ structure.c
@@ -1,34 +1,22 @@
 /*
-Student NAme :Susan Marikhu
-Subject:Programming Fundamental
-LAb No.:23
-Program:Wap to create a STRUCTURE assignment value to its and display the value.
-Date:3 feb,2017
+Student NAme:susan marikhu
+subject:Programming Fundamental
+LAb NO.25
+PRogram:Wap to structure.
+Date :06 Feb,2017
 */
+	
 #include<stdio.h>
-struct student
-{
-    int student_id;
-    int registration_id;
-    float fee;
-
+struct student {
+int id;
+int registration_number	;
+float fee;
 };
 int main(){
 struct student susan;
-susan.student_id=101;
-susan.registration_id=1005;
-susan.fee=10000.50;
-printf("susan id = %d\n",susan.student_id);
-printf("susan registration id = %d\n",susan.registration_id);
-printf("susan fee=%f\n",susan.fee);
-printf("\n");
-struct student sambhav;
-sambhav.student_id=102;
-sambhav.registration_id=1006;
-sambhav.fee=10000.50;
-printf("sambhav id = %d\n",sambhav.student_id);
-printf("sambhav registration id = %d\n",sambhav.registration_id);
-printf("sambhav fee=%f\n",sambhav.fee);
-
-return 0;
-}
+susan.id=100;
+susan.registration_number=10050;
+susan.fee=555.50;
+printf("susan id =%d \n susan registration no. =%d \n susan fee =%f", susan.id,susan.registration_number, susan.fee );
+		return 0;
+		}
