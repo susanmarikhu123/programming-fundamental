71a68e2a616e73609c2331fac6d78e5863df8435
susanmarikhu123 <susan.marikhu123@gmail.com>
2017-02-13 13:18:22 +0545

    Add files via upload

---
+++ wap to read data from a file.c
@@ -0,0 +1,20 @@
+/*
+Student Name:Susan Marikhu
+Subject:Programming Fundamental
+Lab no:26
+Program:Wap to read data from a file.
+Date: 13 feb  2017
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+FILE*fp;
+char data[255];
+fp=fopen("E:/sunway/test.txt","r");
+fgets(data,255,(FILE*)fp);
+fscanf(fp,"%s",data);
+printf("%s",data);
+printf("\n");
+fclose(fp);
+return 0;
+}
